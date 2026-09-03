77ccc0876918a9a0639577bc1427b7a07e1e19c3
savabondarenko2016 <savabondarenko2016@gmail.com>
2026-09-03 21:35:38 +0300

    CalculatePrice_StandartHurs_ReturnsBestPrice - red

---
+++ BusinessLogic.Tests/PriceCalculatorServiceTests.cs
@@ -0,0 +1,21 @@
+﻿namespace BusinessLogic.Tests;
+
+public class PriceCalculatorServiceTests
+{
+    [Fact]
+    public void CalculatePrice_StandartHurs_ReturnsBestPrice()
+    {
+        // Arrange
+        var calculator = new PriceCalculatorService();
+        var basePrice = 2000m;
+
+        var startTime = new DateTime(2024, 9, 1, 10, 0, 0);
+        var endTime = new DateTime(2024, 9, 1, 12, 0, 0);
+
+        // Act
+        var result = calculator.Calculate(basePrice, startTime, endTime);
+
+        // Assert
+        Assert.Equal(4000m, result);
+    }
+}
