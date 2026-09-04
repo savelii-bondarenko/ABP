347f4e47914eb63218d763caced5bacd58b0489c
savabondarenko2016 <savabondarenko2016@gmail.com>
2026-09-04 17:29:10 +0300

    Make Repository

---
+++ DataAccess/Interfaces/IRoomRepository.cs
@@ -0,0 +1,18 @@
+﻿using DataAccess.Entities;
+
+namespace DataAccess.Interfaces;
+
+public interface IRoomRepository
+{
+    Task<Room> AddAsync(Room room);
+
+    Task<Room?> GetByIdAsync(int id);
+
+    Task<IEnumerable<Room>> GetAllAsync();
+
+    Task UpdateAsync(Room room);
+
+    Task DeleteAsync(int id);
+
+    Task<IEnumerable<Room>> GetAvailableRoomsAsync(DateTime startTime, DateTime endTime, int capacity);
+}
---
+++ DataAccess/Repository/RoomRepository.cs
@@ -0,0 +1,52 @@
+﻿using DataAccess.Entities;
+using DataAccess.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess.Repository;
+
+public class RoomRepository(AppDbContext context) : IRoomRepository
+{
+    public async Task<Room> AddAsync(Room room)
+    {
+        ArgumentNullException.ThrowIfNull(room);
+
+        await context.Rooms.AddAsync(room);
+
+        await context.SaveChangesAsync();
+
+        return room;
+    }
+
+    public async Task DeleteAsync(int id)
+    {
+        await context.Rooms.Where(r => r.Id == id).ExecuteDeleteAsync();
+    }
+
+    public async Task UpdateAsync(Room room)
+    {
+        ArgumentNullException.ThrowIfNull(room);
+
+        context.Rooms.Update(room);
+
+        await context.SaveChangesAsync();
+    }
+
+    public async Task<IEnumerable<Room>> GetAllAsync()
+    {
+        return await context.Rooms.AsNoTracking().ToListAsync();
+    }
+
+    public async Task<Room?> GetByIdAsync(int id)
+    {
+        return await context.Rooms.FindAsync(id);
+    }
+
+    public async Task<IEnumerable<Room>> GetAvailableRoomsAsync(DateTime startTime, DateTime endTime, int capacity)
+    {
+        return await context.Rooms
+            .AsNoTracking()
+            .Where(r => r.Capacity >= capacity)
+            .Where(r => !r.Bookings.Any(b => b.StartTime < endTime && b.EndTime > startTime))
+            .ToListAsync();
+    }
+}
