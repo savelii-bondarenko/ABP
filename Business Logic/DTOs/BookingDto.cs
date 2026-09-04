acb0ea95ca3f3df3c7f46dc242b2c2ef8bd986f1
savabondarenko2016 <savabondarenko2016@gmail.com>
2026-09-04 21:14:07 +0300

    Add Bookign DTO

---
+++ Business Logic/DTOs/BookingDto.cs
@@ -0,0 +1,31 @@
+﻿namespace BusinessLogic.DTOs;
+
+public record BookingDto(
+    int Id,
+    int RoomId,
+    DateTime StartTime,
+    DateTime EndTime,
+    decimal TotalPrice
+);
+
+public record CreatBookingeDto(
+    int RoomId,
+    DateTime StartTime,
+    DateTime EndTime,
+    decimal TotalPrice
+);
+
+public record UpdateBookingDto(
+    int RoomId,
+    DateTime StartTime,
+    DateTime EndTime,
+    decimal TotalPrice
+);
+
+public record ResponseBookingDto(
+    int Id,
+    int RoomId,
+    DateTime StartTime,
+    DateTime EndTime,
+    decimal TotalPrice
+);
