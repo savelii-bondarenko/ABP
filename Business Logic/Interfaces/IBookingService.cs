120d2085af14462eca8193c3345f53064acbb943
savabondarenko2016 <savabondarenko2016@gmail.com>
2026-09-04 21:10:05 +0300

    Add IBookingService

---
+++ Business Logic/Interfaces/IBookingService.cs
@@ -0,0 +1,18 @@
+﻿using DataAccess.Entities;
+
+namespace BusinessLogic.Interfaces;
+
+public interface IBookingService
+{
+    Task<Booking> AddAsync(Booking booking);
+
+    Task DeleteAsync(int id);
+
+    Task UpdateAsync(Booking booking);
+
+    Task<Booking?> GetByIdAsync(int id);
+
+    Task<IEnumerable<Booking>> GetAllAsync();
+
+    Task<IEnumerable<Booking>> GetOverlappingBookingsAsync(int roomId, DateTime start, DateTime end);
+}
